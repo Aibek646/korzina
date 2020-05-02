8dabb38b5d56a887b29ffdd25f4c5b916c8964c1
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2020-05-02 14:34:27 -0700

    routes, home, men, login

---
+++ src/components/Men.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Men extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello men page</h2>
+            </div>
+        );
+    }
+}
+
+export default Men;
---
+++ src/components/noMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const noMatch = () => {
+    return (
+        <div>
+            <h2>no Match</h2>
+        </div>
+    );
+}
+
+export default noMatch;
